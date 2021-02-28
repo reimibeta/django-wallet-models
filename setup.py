06dcfd9a735e94a0f5c24efd1f1a6d7d98d3d5da
reimibeta <65539218+reimibeta@users.noreply.github.com>
2021-02-28 21:54:28 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import setuptools
 
 setuptools.setup(
     name='wallet_models',
-    version='1.0.0',
+    version='1.0.1',
     packages=setuptools.find_packages(),
     # scripts=['manage.py']
 )
