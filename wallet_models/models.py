74a2837baf59e1b1953d6e5eeb76f5116d03e021
reimibeta <pinsoneatra.soneatra846@gmail.com>
2021-04-14 11:47:35 +0700

    Update models.py

--- wallet_models/models.py
+++ wallet_models/models.py
@@ -1,9 +1,6 @@
 # wallet baka
 from .wallet_models.wallet import *
 
-# wallet adjust
-from .wallet_models.wallet_adjust import *
-
 # wallet currency
 from .wallet_models.wallet_currency import *
 
