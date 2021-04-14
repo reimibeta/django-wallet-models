32f4c72bfc40ab1e0f7d3be44edb3ef871f0380e
reimibeta <pinsoneatra.soneatra846@gmail.com>
2021-04-14 11:49:06 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import setuptools
 
 setuptools.setup(
     name='wallet_models',
-    version='1.0.7',
+    version='1.0.8',
     packages=setuptools.find_packages(),
     # scripts=['manage.py']
 )
