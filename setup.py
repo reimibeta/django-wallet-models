de6341f52be3583ca93670969ec90f09fb1ec366
reimibeta <pinsoneatra.soneatra846@gmail.com>
2021-05-25 13:33:24 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,16 @@ import setuptools
 
 setuptools.setup(
     name='wallet_models',
-    version='1.1.1',
+    version='1.1.2',
     packages=setuptools.find_packages(),
+    install_requires=[
+        'datetime-utils @ git+https://github.com/reimibeta/django-datetime-utils.git',
+        'html-render-utils @ git+https://github.com/reimibeta/django-html-render-utils.git',
+        'djangorestframework==3.12.4',
+        'djangorestframework-simplejwt==4.7.0',
+        'drf-flex-fields==0.9.0',
+        'django-admin-list-filter-dropdown==1.0.3',
+        'rest-framework-utils @ git+https://github.com/reimibeta/django-rest-framework-utils.git',
+    ]
     # scripts=['manage.py']
 )
